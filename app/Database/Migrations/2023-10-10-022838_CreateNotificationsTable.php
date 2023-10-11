a2ec6472237f96c6862fb0fe577aca4558f8602f
zexpher <fahriangga30@gmail.com>
2023-10-11 22:08:34 +0700

    Tambah field readed_at di notification

--- app/Database/Migrations/2023-10-10-022838_CreateNotificationsTable.php
+++ app/Database/Migrations/2023-10-10-022838_CreateNotificationsTable.php
@@ -14,6 +14,7 @@ class CreateNotificationsTable extends Migration
             'model_id' => ['type' => 'INT', 'constraint' => 11],
             'model_class' => ['type' => 'VARCHAR', 'constraint' => 255],
             'type' => ['type' => 'ENUM', 'constraint' => ['t_reply', 't_like']], // t = thread.
+            'readed_at' => ['type' => 'datetime', 'null' => true],
             'created_at datetime default current_timestamp',
             'updated_at datetime default current_timestamp on update current_timestamp',
         ]);
