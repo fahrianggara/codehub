75186dd2a8beeb99472642dd7c72fc0bf595a6f0
dimasyusuf14 <yusufdimas02@gmail.com>
2023-10-13 21:59:23 +0700

    masukin icon ke input box

--- app/Views/auth/register copy.php
+++ app/Views/auth/register copy.php
@@ -6,27 +6,35 @@
     <div class="row justify-content-center align-items-center vh-100">
         <div class="col-md-6 col-lg-6 col-xl-6">
             <form class="card" action="/register" method="post">
+                <?php csrf_field(); ?>
 
                 <div class="card-header">
                     <h4 class="m-0">Register</h4>
                 </div>
 
                 <div class="card-body">
-                    <div class="alert alert-default-danger" role="alert">
-                    </div>
+
+                    <?php if (isset($validation)): ?>
+                        <div class="alert alert-default-danger" role="alert">
+                            <?= $validation->listErrors() ?>
+                        </div>
+                    <?php endif; ?>
 
                     <div class="row">
+
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="first_name">Nama Awalan</label>
-                                <input type="text" class="form-control" id="first_name" placeholder="Masukkan nama awalan.." name="first_name" value="<?= old('first_name') ?>">
+                                <input type="text" class="form-control" id="first_name" placeholder="Masukkan nama awalan.."
+                                    name="first_name" value="<?= old('first_name') ?>">
                             </div>
                         </div>
 
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="last_name">Nama Akhiran</label>
-                                <input type="text" class="form-control" id="last_name" placeholder="Masukkan nama akhiran.." name="last_name" value="<?= old('last_name') ?>">
+                                <input type="text" class="form-control" id="last_name" placeholder="Masukkan nama akhiran.."
+                                    name="last_name" value="<?= old('last_name') ?>">
                             </div>
                         </div>
 
@@ -34,12 +42,14 @@
 
                     <div class="form-group">
                         <label for="username">Username</label>
-                        <input type="text" class="form-control" id="username" placeholder="Masukkan username.." name="username" value="<?= old('username') ?>">
+                        <input type="text" class="form-control" id="username" placeholder="Masukkan username.."
+                            name="username" value="<?= old('username') ?>">
                     </div>
 
                     <div class="form-group">
                         <label for="email">Email</label>
-                        <input type="email" class="form-control" id="email" placeholder="Masukkan email kamu.." name="email" value="<?= old('email') ?>">
+                        <input type="email" class="form-control" id="email" 
+                            placeholder="Masukkan email kamu.." name="email" value="<?= old('email') ?>">
                     </div>
 
                     <div class="row">
@@ -47,13 +57,15 @@
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label for="password">Password</label>
-                                <input type="password" class="form-control" id="password" placeholder="Masukkan password.." name="password">
+                                <input type="password" class="form-control" id="password" placeholder="Masukkan password.."
+                                    name="password">
                             </div>
                         </div>
 
                         <div class="col-md-6 mb-0">
                             <label for="confirmpassword">Confirmation</label>
-                            <input type="password" class="form-control" id="confirmpassword" placeholder="Konfirmasi Password.." name="confirmpassword">
+                            <input type="password" class="form-control" id="confirmpassword"
+                                placeholder="Konfirmasi Password.." name="confirmpassword">
                         </div>
 
                     </div>
