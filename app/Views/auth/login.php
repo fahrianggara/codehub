f358a1b9d343f538b0b39584f92cf823eda4aeb7
zexpher <fahriangga30@gmail.com>
2023-10-14 01:16:37 +0700

    Edit layout login sama register sedikit..

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -3,64 +3,62 @@
 <?= $this->section('content'); ?>
 
 <?php
-$flashDataUsername = session()->getFlashdata('errUsername');
-$flashDataPassword = session()->getFlashdata('errPassword');
+    $flashDataUsername = session()->getFlashdata('errUsername');
+    $flashDataPassword = session()->getFlashdata('errPassword');
 ?>
 
+<!-- <div class="logo">
+    <img src="<?= base_url('images/logo/codehub.png') ?>" alt="Logo">
+</div> -->
+
 <div class="wrapper">
-        <form action="">
-    
-            <div class="header">
-                <h1>Login</h1>
+
+    <form action="">
+        <div class="header">
+            <h1>Login</h1>
+        </div>
+
+        <div class="input-box">
+            <input type="text" class="" placeholder="Username atau Email"
+                name="username" id="username" value="<?= old('username'); ?>">
+            <i class="bi bi-person-fill"></i>
+        </div>
+
+        <div class="input-box">
+            <input type="password" class="" id="password" placeholder="Password" name="password">
+            <i class="bi bi-lock-fill"></i>
+
+            <div class="hide-pw">
+                <span id="show-password" class="show-password" style="display:none;"
+                    onclick="togglePasswordVisibility()">
+                    <i class="bi bi-eye-slash"></i>
+                </span>
             </div>
-    
-            <div class="input-box">
-                <input type="text" placeholder="Username" required>
-                <i class="bi bi-person-fill"></i>
-            </div>
-    
-            <div class="input-box">
-                <input type="password" id="password" placeholder="Password" required>
-                <i class="bi bi-lock-fill"></i>
-               
-                <div class="hide-pw">
-                    <span id="show-password" class="show-password" onclick="togglePasswordVisibility()">
-                        <i class="bi bi-eye-slash"></i></span>
-                </div>
-            </div>
-    
-            <div class="remember-forgot">
-                <a href="#">Lupa Password?</a>
-            </div>
-    
-            <button type="submit" class="button">Login</button>
-    
-            <div class="register-link">
-                <p>Belum punya akun? <a href="/register">Register</a></p>
-            </div>
-            
-        </form>
-    
+        </div>
+
+        <div class="remember-forgot">
+            <a href="#">Lupa Password?</a>
+        </div>
+
+        <button type="submit" class="button">Submit</button>
+
+        <div class="register-link">
+            <p>Belum punya akun? <a href="/register">Register</a></p>
+        </div>
+        
+    </form>
 </div>
-
-
 
 <?= $this->endSection(); ?>
 
 <?= $this->section('js'); ?>
+
 <script>
     $(document).ready(function() {
-        $("#show-password").hide(); // Sembunyikan logo mata awalnya
-
         $("#password").on("input", function() {
             var passwordField = $("#password");
             var showPasswordIcon = $("#show-password");
-
-            if (passwordField.val() !== "") {
-                showPasswordIcon.show();
-            } else {
-                showPasswordIcon.hide();
-            }
+            passwordField.val() !== "" ? showPasswordIcon.show() : showPasswordIcon.hide();
         });
 
         $("#show-password").on("click", function() {
@@ -77,6 +75,5 @@ $flashDataPassword = session()->getFlashdata('errPassword');
         });
     });
 </script>
-
 
 <?= $this->endSection(); ?>
--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -2,59 +2,109 @@
 
 <?= $this->section('content'); ?>
 
-    <div class="wrapper">
-        <form action="">
-    
-            <div class="header">
-                <h1>Register</h1>
-            </div>
-    
-            <div class="input-box">
-                <input type="text" placeholder="Masukan email" required>
-                <i class="bi bi-envelope-fill"></i>
-            </div>
-    
-            <div class="input-box">
-                <input type="password" id="password" placeholder="Masukan password" required>
-                <i class="bi bi-lock-fill"></i>
-               
-                <div class="hide-pw">
-                    <span id="show-password" class="show-password">
-                        <i class="bi bi-eye-slash"></i></span>
-                </div>
-            </div>
+<!-- <div class="logo">
+    <img src="<?= base_url('images/logo/codehub.png') ?>" alt="Logo">
+</div> -->
 
-            <div class="input-box">
-                <input type="password" id="konfirm-pass" placeholder="Konfirmasi password" required>
-                <i class="bi bi-lock-fill"></i>
-            </div>
-    
-            <button type="submit" class="button">Submit</button>
-    
-            <div class="register-link">
-                <p>Sudah punya akun? <a href="/login">Login</a></p>
-            </div>
-            
-        </form>
-    
-    </div>
+<div class="wrapper">
+    <form id="form-register" action="<?= base_url('register') ?>" method="POST">
+        <?= csrf_field(); ?>
 
-    <?= $this->endSection(); ?>
-   
-    <?= $this->section('js'); ?>
-    <script>
-        $(document).ready(function() {
-            $('#show-password').click(function () {
-        var inputFields = $('#password, #konfirm-pass');
-        var icon = $(this).find('i');
-        var isPasswordVisible = (inputFields.attr('type') === 'password');
+        <div class="header">
+            <h1>Register</h1>
+        </div>
 
-        inputFields.attr('type', isPasswordVisible ? 'text' : 'password');
-        icon.removeClass(isPasswordVisible ? 'bi-eye-slash' : 'bi-eye')
-            .addClass(isPasswordVisible ? 'bi-eye' : 'bi-eye-slash');
-    });
+        <div class="input-box">
+            <input type="text" placeholder="Masukan email" name="email">
+            <i class="bi bi-envelope-fill"></i>
+        </div>
+
+        <div class="input-box">
+            <input type="password" id="password" placeholder="Masukan password" name="password">
+            <i class="bi bi-lock-fill"></i>
+
+            <div class="hide-pw">
+                <span id="show-password" class="show-password">
+                    <i class="bi bi-eye-slash"></i></span>
+            </div>
+        </div>
+
+        <div class="input-box">
+            <input type="password" id="konfirm-pass" placeholder="Konfirmasi password" name="confirmpass">
+            <i class="bi bi-lock-fill"></i>
+        </div>
+
+        <button type="submit" class="button">Submit</button>
+
+        <div class="register-link">
+            <p>Sudah punya akun? <a href="/login">Login</a></p>
+        </div>
+
+    </form>
+
+</div>
+
+<?= $this->endSection(); ?>
+
+<?= $this->section('js'); ?>
+
+<script>
+    $(document).ready(function () {
+        const form = $('#form-register');
+        const submit = form.find('button[type="submit"]');
+
+        // ajax register
+        form.on("submit", function (e) {
+            e.preventDefault();
+
+            $.ajax({
+                type: $(this).attr("method"),
+                url: $(this).attr("action"),
+                data: $(this).serialize(),
+                dataType: "JSON",
+                beforeSend: function () {
+                    submit.attr("disabled", "disabled");
+                    submit.html('Loading..');
+                },
+                complete: function () {
+                    submit.removeAttr("disabled");
+                    submit.html('Submit');
+                },
+                success: function (res) {
+                    if (res.status === 400) {
+                        errors = [];
+
+                        $.each(res.error, function (key, val) {
+                            errors.push(val);
+                        });
+
+                        alertify.alert(errors.join("<br><br>"));
+                        $(document).find(".alertify .msg").addClass("text-danger");
+                    } else {
+                        alertify.alert(res.message, function () {
+                            window.location.href = "/login";
+                        });
+                        $(document).find(".alertify .msg").addClass("text-success");
+                    }
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
         });
-    </script>
-    
+
+        // Show password
+        $('#show-password').click(function () {
+            var inputFields = $('#password, #konfirm-pass');
+            var icon = $(this).find('i');
+            var isPasswordVisible = (inputFields.attr('type') === 'password');
+
+            inputFields.attr('type', isPasswordVisible ? 'text' : 'password');
+            icon.removeClass(isPasswordVisible ? 'bi-eye-slash' : 'bi-eye')
+                .addClass(isPasswordVisible ? 'bi-eye' : 'bi-eye-slash');
+        });
+    });
+</script>
+
 
 <?= $this->endSection(); ?>
