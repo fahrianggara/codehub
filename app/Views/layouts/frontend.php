b29a9fade510dfa212c01206f43d3264328a77b0
zexpher <fahriangga30@gmail.com>
2023-10-14 16:50:18 +0700

    Membuat frontend layout

---
+++ app/Views/layouts/frontend.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= "$title - CodeHUB" ?></title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="<?= base_url('plugins/bootstrap4/bootstrap.min.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('plugins/alertify/css/alerts.css') ?>">
+    <link rel="stylesheet" href="<?= base_url('css/frontend.css') ?>">
+
+    <!-- jQuery -->
+    <script src="<?= base_url('plugins/jquery/jquery.min.js') ?>"></script>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <?php $this->include('layouts/frontend/navbar'); ?>
+
+    <!-- Content -->
+    <?php $this->renderSection('content'); ?>
+
+    <!-- Footer -->
+    <?php $this->include('layouts/frontend/footer'); ?>
+
+    <!-- JS -->
+    <script src="<?= base_url('plugins/bootstrap4/bootstrap.bundle.min.js') ?>"></script>
+    <script src="<?= base_url('plugins/alertify/js/alerts.js') ?>"></script>
+    <script src="<?= base_url('js/frontend.js') ?>"></script>
+</body>
+
+</html>
