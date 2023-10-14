47f3a5e0b242d373387015b1720ade45d48f7bdf
zexpher <fahriangga30@gmail.com>
2023-10-14 16:53:07 +0700

    Menambah route profile dan filtering guest

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -2,6 +2,7 @@
 
 namespace Config;
 
+use App\Filters\Guest;
 use CodeIgniter\Config\BaseConfig;
 use CodeIgniter\Filters\CSRF;
 use CodeIgniter\Filters\DebugToolbar;
@@ -24,6 +25,7 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'guest'         => Guest::class,
     ];
 
     /**
--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,9 +9,11 @@ use CodeIgniter\Router\RouteCollection;
 $routes->get('/', 'Home::index');
 
 // Auth Login & Register
-$routes->group('', ['namespace' => Auth::class], function (RouteCollection $routes) {
+$routes->group('', ['filter' => 'guest', 'namespace' => Auth::class], function (RouteCollection $routes) {
     $routes->get('login', 'LoginController::index');
     $routes->post('login', 'LoginController::login');
     $routes->get('register', 'RegisterController::index');
     $routes->post('register', 'RegisterController::register');
 });
+
+$routes->get('(:any)', 'ProfileController::index/$1');
