58e5d07cd4e0b5100087021df6c3ea0e68373ae8
zexpher <fahriangga30@gmail.com>
2023-10-14 16:53:29 +0700

    Tambahkan kata CodeHUB di title

--- app/Views/layouts/auth.php
+++ app/Views/layouts/auth.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title ?></title>
+    <title><?= "$title - CodeHUB" ?></title>
 
     <!-- CSS File -->
     <link rel="stylesheet" href="<?= base_url('plugins/bootstrap4/bootstrap.min.css') ?>">
