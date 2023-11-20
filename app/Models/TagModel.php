d6e68c33cad44515a4215f41276b7cbb0e15f6a4
dimasyusuf14 <yusufdimas02@gmail.com>
2023-11-20 11:36:06 +0700

    upload bakend tags

--- app/Models/TagModel.php
+++ app/Models/TagModel.php
@@ -11,6 +11,32 @@ class TagModel extends Model
     protected $returnType       = Tag::class;
     protected $useTimestamps    = true;
     protected $allowedFields    = [
-        'name', 'slug'
+        'name', 'slug',
     ];
+
+    public function getTags()
+    {
+        return $this->findAll();
+    }
+
+    public function getTag($id)
+    {
+        return $this->find($id);
+    }
+
+    public function createTag($data)
+    {
+        return $this->insert($data);
+    }
+
+    public function updateTag($id, $data)
+    {
+        return $this->update($id, $data);
+    }
+
+    public function deleteTag($id)
+    {
+        return $this->delete($id);
+    }
 }
+
---
+++ app/Views/backend/tags/create.php
@@ -0,0 +1,52 @@
+<?= $this->extend('layouts/backend') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container-fluid">
+    <div class="row align-items-center justify-content-center">
+        <div class="col-xl-9 col-lg-12">
+
+            <form id="formCreate" action="<?= route_to('admin.Tags.store') ?>" class="card" autocomplete="off" method="post">
+
+                <input type="hidden" name="_method" value="POST">
+                <?= csrf_field() ?>
+
+                <div class="card-header p-2">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <span class="ml-2 py-1">Form Tambah Tag</span>
+                    </div>
+                </div>
+
+                <div class="card-body">
+
+                    <div class="row">
+                        <div class="form-group col-md-6">
+                            <label for="name">Nama Tag</label>
+
+                            <input type="text" name="name" id="name" class="form-control <?= validation_show_error('name') ? 'is-invalid' : '' ?>" value="<?= old('name') ?>" placeholder="Masukkan Nama Tag">
+
+                            <div class="invalid-feedback d-block">
+                                <?= validation_show_error('name') ?>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card-footer p-2 ">
+                    <div class="d-flex align-items-center">
+                        <a href="<?= route_to('admin.Tags') ?>" class="btn btn-sm btn-primary">
+                            <i class="fas fa-arrow-left mr-1"></i> Kembali
+                        </a>
+                        <button type="submit" class="btn btn-sm btn-success ml-1">
+                            Simpan Data
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection() ?>
