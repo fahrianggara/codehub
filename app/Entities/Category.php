a1c0460b804a549d7d7c23b686a30ea0b49b4855
dajaru <117800923+Dajaru@users.noreply.github.com>
2023-11-26 17:28:35 +0700

    create, kategori index, routes dan controller

--- app/Entities/Category.php
+++ app/Entities/Category.php
@@ -9,4 +9,15 @@ class Category extends Entity
     protected $datamap = [];
     protected $dates   = ['created_at', 'updated_at'];
     protected $casts   = [];
+
+  /**
+     * Get photo attribute
+     * 
+     * @return void
+     */
+    public function getPhoto()
+    {
+        $path = 'images/categories/' . $this->attributes['cover'];
+        return file_exists($path) ? base_url($path) : base_url('images/empty.png');
+    }
 }
---
+++ app/Views/backend/kategori/index.php
@@ -0,0 +1,107 @@
+<?= $this->extend('layouts/backend') ?>
+<?= $this->section('content') ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header p-2">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <span class="ml-2">Daftar Kategori di CODEHUB</span>
+                        <a href="<?= route_to('admin.kategori.create') ?>" class="btn btn-sm btn-success">
+                            <i class="fas fa-plus mr-1"></i>
+                            Tambah
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body table-responsive">
+                    <table id="tablePengguna" class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Nama</th>
+                                <th>slug</th>
+                                <th>cover</th>
+                                <th>Dibuat Pada</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $no = 1; ?>
+                            <?php foreach ($categories as $category) : ?>
+                                <?php $id = base64_encode($category->id); ?>
+                                <tr>
+                                    <td><?= $no++ ?></td>
+                                    <td><?= $category->name ?></td>
+                                    <td><?= $category->slug ?></td>
+                                    <td>
+                                        <img src="<?= $category->photo ?>" alt="" class="img-fluid" width="80">
+                                    </td>
+                                    <td><?= waktu($category->created_at, 'l, d F Y', false) ?></td>
+                                    <td>
+                                        <div class="btn-group dropleft">
+                                            <button class="btn btn-sm btn-more dropdown-toggle" data-toggle="dropdown" aria-expanded="false" data-display="static">
+                                                <i class="fas fa-ellipsis-v"></i>
+                                            </button>
+                                            <div class="dropdown-menu dropdown-menu-right">
+                                                <a href="<?= route_to('admin.laporan.edit', $id) ?>" class="dropdown-item py-1">
+                                                    <i class="fas text-warning fa-pen mr-2"></i> Edit
+                                                </a>
+                                                <button type="button" value="<?= $id ?>" class="dropdown-item py-1 btn-delete" data-id="<?= $category->id ?>" data-action="<?= route_to('admin.laporan.destroy') ?>">
+                                                    <i class="fas text-danger fa-trash mr-2"></i> Hapus
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?= $this->endSection() ?>
+<?= $this->section('js') ?>
+<script>
+    const btnDelete = $(".btn-delete");
+    const table = $("#tablePengguna").DataTable({
+        language: {
+            url: `${origin}/plugins/datatables/datatables-language/idn.json`
+        },
+    });
+    btnDelete.on("click", function(e) {
+        e.preventDefault();
+        const id = $(this).val();
+        const username = $(this).data("username");
+        const message = `Apakah kamu yakin ingin menghapus pengguna <strong>${username}</strong>?`;
+        const confirm = (e) => {
+            e.preventDefault();
+            $.ajax({
+                url: $(this).data("action"),
+                type: "POST",
+                data: {
+                    id
+                },
+                dataType: "JSON",
+                success: (res) => {
+                    if (res.status === 400) {
+                        alertError(res.message);
+                    } else {
+                        alertifyLog('success', res.message, () => {
+                            location.reload();
+                        });
+                    }
+                },
+                error: (xhr, status, error) => {
+                    alertError("Maaf, terjadi kesalahan pada server. Lihat di console untuk detailnya.");
+                    console.log(xhr.responseText);
+                }
+            });
+        }
+        alertifyConfirm(message, confirm, (e) => {
+            $('body').removeClass('modal-open');
+        });
+    });
+</script>
+<?= $this->endSection()  ?>
